0fa5cd463c3daa7381e79c672e84144f90dc0a3e
Svetlana Georgieva <sv.georgieva@pgzaimov.com>
2023-01-12 10:35:53 +0200

    Created all pages publicly

---
+++ Freelancer/Controllers/JobsUserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Freelancer.Models;
+using Freelancer.Data;
+
+namespace Freelancer.Controllers
+{
+    public class JobsUserController : Controller
+    {
+        private readonly FreelancerContext _context;
+        public JobsUserController(FreelancerContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Job.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Job == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _context.Job
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+    }
+}
